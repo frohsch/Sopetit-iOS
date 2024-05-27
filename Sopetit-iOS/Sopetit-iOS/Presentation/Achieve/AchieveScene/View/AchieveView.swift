0128889b5e7eae0b8a97434b22afbb99fa57b3ff
ahra1221 <lizze011221@gmail.com>
2024-05-27 12:50:44 +0900

    [Fix] #130 - 폴더링 변경 및 불필요한 파일 삭제

---
+++ Sopetit-iOS/Sopetit-iOS/Presentation/Achieve/AchieveScene/View/AchieveView.swift
@@ -0,0 +1,42 @@
+//
+//  AchieveView.swift
+//  Sopetit-iOS
+//
+//  Created by 고아라 on 5/27/24.
+//
+
+import UIKit
+
+import SnapKit
+
+final class AchieveView: UIView {
+    
+    // MARK: - UI Components
+    
+    // MARK: - Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setHierarchy()
+        setLayout()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Extensions
+
+private extension AchieveView {
+    
+    func setHierarchy() {
+        
+    }
+    
+    func setLayout() {
+        
+    }
+}
