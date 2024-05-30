c3eb3d8642b706aa815c1118cd3cd113b755b555
ahra1221 <lizze011221@gmail.com>
2024-05-30 21:31:41 +0900

    [Chore] #130 - 컬러 변경사항 반영

--- Sopetit-iOS/Sopetit-iOS/Global/Literals/ColorLiterals.swift
+++ Sopetit-iOS/Sopetit-iOS/Global/Literals/ColorLiterals.swift
@@ -12,75 +12,71 @@ extension UIColor {
         return UIColor(hex: "#FFFFFF")
     }
     
-    static var SoftieMain1: UIColor {
-        return UIColor(hex: "#4A4A4A")
-    }
-    
-    static var SoftieMain2: UIColor {
-        return UIColor(hex: "#FFF7E6")
-    }
-    
     static var SoftieRed: UIColor {
         return UIColor(hex: "#F76D6D")
     }
     
-    static var SoftieYellow: UIColor {
-        return UIColor(hex: "#FEE500")
+    static var Purple200: UIColor {
+        return UIColor(hex: "#8e77f5")
     }
     
-    static var SoftieBlue: UIColor {
-        return UIColor(hex: "#7B89D1")
+    static var Purple0: UIColor {
+        return UIColor(hex: "#AE9CFF")
     }
     
-    static var SoftieBackHome: UIColor {
-        return UIColor(hex: "#FBF9F1")
+    static var Purple100: UIColor {
+        return UIColor(hex: "#E09EFF")
     }
     
-    static var SoftieBack: UIColor {
+    static var Brown100: UIColor {
+        return UIColor(hex: "#d4bba5")
+    }
+    
+    static var Brown200: UIColor {
+        return UIColor(hex: "#c4ac98")
+    }
+    
+    static var Gray50: UIColor {
         return UIColor(hex: "#F8F8F8")
     }
     
-    static var SoftieHomeFill: UIColor {
-        return UIColor(hex: "#D4BBA5")
-    }
-    
-    static var SoftieHomeStroke: UIColor {
-        return UIColor(hex: "#C4AC98")
-    }
-    
-    static var SoftieBrown: UIColor {
-        return UIColor(hex: "#C0804C")
-    }
-    
-    static var Gray000: UIColor {
+    static var Gray100: UIColor {
         return UIColor(hex: "#F4F4F4")
     }
     
-    static var Gray100: UIColor {
-        return UIColor(hex: "#E9EBED")
-    }
-    
-    static var Gray200: UIColor {
+    static var Gray300: UIColor {
         return UIColor(hex: "#CACDD2")
     }
     
-    static var Gray300: UIColor {
+    static var Gray400: UIColor {
         return UIColor(hex: "#9FA4A9")
     }
     
-    static var Gray400: UIColor {
+    static var Gray500: UIColor {
         return UIColor(hex: "#73787E")
     }
     
-    static var Gray500: UIColor {
+    static var Gray600: UIColor {
         return UIColor(hex: "#464C52")
     }
     
-    static var Gray600: UIColor {
-        return UIColor(hex: "#26282B")
+    static var Gray650: UIColor {
+        return UIColor(hex: "#4a4a4a")
     }
     
     static var Gray700: UIColor {
+        return UIColor(hex: "#26282B")
+    }
+    
+    static var Gray800: UIColor {
         return UIColor(hex: "#1B1D1F")
+    }
+    
+    static var Gray950: UIColor {
+        return UIColor(hex: "#000000").withAlphaComponent(0.5)
+    }
+    
+    static var Gray1000: UIColor {
+        return UIColor(hex: "#000000").withAlphaComponent(0.7)
     }
 }
